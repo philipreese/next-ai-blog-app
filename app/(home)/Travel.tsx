c4ca962c61735edd46e91468216b5c01ed5b9a72
Philip Reese <philipreese@mail.com>
2024-09-07 15:12:26 -0400

    add travel section of main

---
+++ app/(home)/Travel.tsx
@@ -0,0 +1,28 @@
+import Card from "app/(shared)/Card";
+
+const Travel = () => {
+  return (
+    <section className="mt-10">
+      <hr className="border-1" />
+      {/* HEADER */}
+      <div className="flex items-center gap-3 my-8">
+        <h4 className="bg-accent-green py-2 px-5 text-wh-900 text-sm font-bold">
+          TRAVEL
+        </h4>
+        <p className="font-bold text-2xl">New Travel Experiences</p>
+      </div>
+
+      {/* CARDS ROW */}
+      <div className="sm:flex justify-between gap-8">
+        <Card className="basis-1/3 bg-wh-500 mt-5 sm:mt-0" imageHeight="h-80" />
+        <Card className="basis-1/3 bg-wh-500 mt-5 sm:mt-0" imageHeight="h-80" />
+        <Card className="basis-1/3 bg-wh-500 mt-5 sm:mt-0" imageHeight="h-80" />
+      </div>
+      <Card
+        className="bg-wh-500 sm:flex justify-between items-center gap-3 mt-7 mb-5"
+        imageHeight="h-80"
+      />
+    </section>
+  );
+};
+export default Travel;
