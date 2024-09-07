21aceb858bdcb547370c4b7b3887a345696e54d7
Philip Reese <philipreese@mail.com>
2024-09-07 15:34:22 -0400

    add other section of main

---
+++ app/(shared)/Other.tsx
@@ -0,0 +1,18 @@
+import Card from "./Card";
+
+const Other = () => {
+  return (
+    <section className="mt-10">
+      <hr className="border-1" />
+      {/* HEADER */}
+      <p className="font-bold text-2xl my-8">Other Trending Posts</p>
+      <div className="sm:grid grid-cols-2 gap-10">
+        <Card className="bg-wh-500 mt-5 sm:mt-0" imageHeight="h-80" />
+        <Card className="bg-wh-500 mt-5 sm:mt-0" imageHeight="h-80" />
+        <Card className="bg-wh-500 mt-5 sm:mt-0" imageHeight="h-80" />
+        <Card className="bg-wh-500 mt-5 sm:mt-0" imageHeight="h-80" />
+      </div>
+    </section>
+  );
+};
+export default Other;
